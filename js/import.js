900bf06719a18658922adade3c5257e631efdddb
Eunice Dhivya <divi.seed@gmail.com>
2020-07-07 16:54:35 +0530

    update us name in data

--- js/import.js
+++ js/import.js
@@ -9,7 +9,7 @@ var alldata = [
     },
     {
       "Candidate": "Bacillus Calmette-Guerin (BCG) live-attenuated vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "AZD1222",
@@ -17,11 +17,11 @@ var alldata = [
     },
     {
       "Candidate": "mRNA-1273",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "BNT162",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "BNT162",
@@ -53,7 +53,7 @@ var alldata = [
     },
     {
       "Candidate": "INO-4800",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "mRNA-based vaccine",
@@ -77,11 +77,11 @@ var alldata = [
     },
     {
       "Candidate": "PittCoVacc",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Measles vector vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Measles vector vaccine",
@@ -89,7 +89,7 @@ var alldata = [
     },
     {
       "Candidate": "NVX-CoV2373",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "NVX-CoV2373",
@@ -97,7 +97,7 @@ var alldata = [
     },
     {
       "Candidate": "Ii-Key peptide COVID-19 vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Recombinant vaccine",
@@ -117,15 +117,15 @@ var alldata = [
     },
     {
       "Candidate": "Ad26.COV2-S",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "AdCOVID",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "T-COVIDTM",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Protein subunit vaccine",
@@ -137,11 +137,11 @@ var alldata = [
     },
     {
       "Candidate": "Recombinant vesicular stomatitis virus (rVSV) vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Adenovirus-based vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Molecular clamp vaccine",
@@ -149,11 +149,11 @@ var alldata = [
     },
     {
       "Candidate": "AAVCOVID",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Recombinant vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     },
     {
       "Candidate": "Covaxin",
@@ -169,6 +169,6 @@ var alldata = [
     },
     {
       "Candidate": "gp96-based vaccine",
-      "Country/ies": "USA"
+      "Country/ies": "United States"
     }
   ]
