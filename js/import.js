c60fae453b31fba647f83507400cd54c24e12160
Eunice Dhivya <divi.seed@gmail.com>
2020-07-07 16:19:39 +0530

    remove unnecessary key, values from alldata variable

--- js/import.js
+++ js/import.js
@@ -1,346 +1,174 @@
 var alldata = [
     {
       "Candidate": "Bacillus Calmette-Guerin (BCG) live-attenuated vaccine",
-      "Sponsor": "University of Melbourne and Murdoch Children’s Research Institute; Radboud University Medical Center; Faustman Lab at Massachusetts General Hospital",
-      "Trial Phase": "Phase 2/3",
-      "Institution": "University of Melbourne and Murdoch Children’s Research Institute; Radboud University Medical Center; Faustman Lab at Massachusetts General Hospital",
-      "Funding": "Murdoch Children’s Research Institute; UMC Utrecht",
       "Country/ies": "Australia"
     },
     {
       "Candidate": "Bacillus Calmette-Guerin (BCG) live-attenuated vaccine",
-      "Sponsor": "University of Melbourne and Murdoch Children’s Research Institute; Radboud University Medical Center; Faustman Lab at Massachusetts General Hospital",
-      "Trial Phase": "Phase 2/3",
-      "Institution": "University of Melbourne and Murdoch Children’s Research Institute; Radboud University Medical Center; Faustman Lab at Massachusetts General Hospital",
-      "Funding": "Murdoch Children’s Research Institute; UMC Utrecht",
       "Country/ies": "Netherlands"
     },
     {
       "Candidate": "Bacillus Calmette-Guerin (BCG) live-attenuated vaccine",
-      "Sponsor": "University of Melbourne and Murdoch Children’s Research Institute; Radboud University Medical Center; Faustman Lab at Massachusetts General Hospital",
-      "Trial Phase": "Phase 2/3",
-      "Institution": "University of Melbourne and Murdoch Children’s Research Institute; Radboud University Medical Center; Faustman Lab at Massachusetts General Hospital",
-      "Funding": "Murdoch Children’s Research Institute; UMC Utrecht",
       "Country/ies": "USA"
     },
     {
       "Candidate": "AZD1222",
-      "Sponsor": "The University of Oxford",
-      "Trial Phase": "Phase 2/3",
-      "Institution": "The University of Oxford, the Jenner Institute",
-      "Funding": "Operation Warp Speed; UK Ministry of Health; The University of Oxford; BARDA",
       "Country/ies": "United Kingdom"
     },
     {
       "Candidate": "mRNA-1273",
-      "Sponsor": "Moderna",
-      "Trial Phase": "Phase 2",
-      "Institution": "Kaiser Permanente Washington Health Research Institute",
-      "Funding": "Operation Ward Speed; NIAID, BARDA ($483 million)",
       "Country/ies": "USA"
     },
     {
       "Candidate": "BNT162",
-      "Sponsor": "Pfizer, BioNTech",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Multiple study sites in Europe and North America",
-      "Funding": "Pfizer; BioNTech",
       "Country/ies": "USA"
     },
     {
       "Candidate": "BNT162",
-      "Sponsor": "Pfizer, BioNTech",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Multiple study sites in Europe and North America",
-      "Funding": "Pfizer; BioNTech",
       "Country/ies": "EU"
     },
     {
       "Candidate": "Inactivated vaccine",
-      "Sponsor": "Wuhan Institute of Biological Products; China National Pharmaceutical Group (Sinopharm)",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Henan Provincial Center for Disease Control and Prevention",
-      "Funding": "Ministry of Science and Technology, China",
       "Country/ies": "China"
     },
     {
       "Candidate": "BBIBP-CorV",
-      "Sponsor": "Beijing Institute of Biological Products; China National Pharmaceutical Group (Sinopharm)",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Henan Provincial Center for Disease Control and Prevention",
-      "Funding": "Ministry of Science and Technology, China",
       "Country/ies": "China"
     },
     {
       "Candidate": "CoronaVac",
-      "Sponsor": "Sinovac",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Sinovac Research and Development Co., Ltd.",
-      "Funding": "Sinovac Research and Development Co., Ltd.",
       "Country/ies": "China"
     },
     {
       "Candidate": "GX-19",
-      "Sponsor": "Genexine",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Genexine",
-      "Funding": "Genexine",
       "Country/ies": "South Korea"
     },
     {
       "Candidate": "Gam-COVID-Vac",
-      "Sponsor": "Gamaleya Research Institute, Acellena Contract Drug Research and Development",
-      "Trial Phase": "Phase 1/2",
-      "Institution": "Various",
-      "Funding": "Gamaleya Research Institute of Epidemiology and Microbiology, Health Ministry of the Russian Federation",
       "Country/ies": "Russia"
     },
     {
       "Candidate": "Ad5-nCoV",
-      "Sponsor": "CanSino Biologics",
-      "Trial Phase": "Phase 1",
-      "Institution": "Tongji Hospital; Wuhan, China",
-      "Funding": "CanSino Biologics",
       "Country/ies": "China"
     },
     {
       "Candidate": "INO-4800",
-      "Sponsor": "Inovio Pharmaceuticals",
-      "Trial Phase": "Phase 1",
-      "Institution": "Center for Pharmaceutical Research, Kansas City. Mo.; University of Pennsylvania, Philadelphia",
-      "Funding": "Inovio Pharmaceuticals",
       "Country/ies": "USA"
     },
     {
       "Candidate": "mRNA-based vaccine",
-      "Sponsor": "CureVac",
-      "Trial Phase": "Phase 1",
-      "Institution": "CureVac",
-      "Funding": "CureVac; German federal government",
       "Country/ies": "Germany"
     },
     {
       "Candidate": "SCB-2019",
-      "Sponsor": "GlaxoSmithKline, Sanofi, Clover Biopharmaceuticals, Dynavax and Xiamen Innovax",
-      "Trial Phase": "Phase 1",
-      "Institution": "Linear Clinical Research (Australia)",
-      "Funding": "CEPI",
       "Country/ies": "Australia "
     },
     {
       "Candidate": "Adjuvant recombinant vaccine candidate",
-      "Sponsor": "Anhui Zhifei Longcom Biopharmaceutical, Institute of Microbiology of the Chinese Academy of Sciences",
-      "Trial Phase": "Phase 1",
-      "Institution": null,
-      "Funding": null,
       "Country/ies": "China"
     },
     {
       "Candidate": "COVAX-19",
-      "Sponsor": "Vaxine Pty Ltd.",
-      "Trial Phase": "Phase 1",
-      "Institution": "Royal Adelaide Hospital",
-      "Funding": "NIAID",
       "Country/ies": "Australia"
     },
     {
       "Candidate": "bacTRL-Spike",
-      "Sponsor": "Symvivo",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Symvivo Corporation",
-      "Funding": "Symvivo Corporation",
       "Country/ies": "Canada"
     },
     {
       "Candidate": "PittCoVacc",
-      "Sponsor": "UPMC/University of Pittsburgh School of Medicine",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "University of Pittsburgh",
-      "Funding": "CEPI",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Measles vector vaccine",
-      "Sponsor": "University of Pittsburgh’s Center for Vaccine Research",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "University of Pittsburgh; Themis Biosciences; Institut Pasteur",
-      "Funding": "Merck",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Measles vector vaccine",
-      "Sponsor": "University of Pittsburgh’s Center for Vaccine Research",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "University of Pittsburgh; Themis Biosciences; Institut Pasteur",
-      "Funding": "Merck",
       "Country/ies": "France"
     },
     {
       "Candidate": "NVX-CoV2373",
-      "Sponsor": "Novavax",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Novavax",
-      "Funding": "Novavax, CEPI, Department of Defense ($60 million)",
       "Country/ies": "USA"
     },
     {
       "Candidate": "NVX-CoV2373",
-      "Sponsor": "Novavax",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Novavax",
-      "Funding": "Novavax, CEPI, Department of Defense ($60 million)",
       "Country/ies": "Sweden"
     },
     {
       "Candidate": "Ii-Key peptide COVID-19 vaccine",
-      "Sponsor": "Generex Biotechnology",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Generex",
-      "Funding": "Generex",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Recombinant vaccine",
-      "Sponsor": "Vaxart",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Vaxart",
-      "Funding": "no information",
       "Country/ies": "xx"
     },
     {
       "Candidate": "Self-amplifying RNA vaccine",
-      "Sponsor": "Imperial College London",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Imperial College London",
-      "Funding": "UK Secretary of State for Health; UK Secretary of State for Business, Energy and Industrial Strategy",
       "Country/ies": "United Kingdom"
     },
     {
       "Candidate": "Plant-based COVID-19 vaccine",
-      "Sponsor": "Medicago",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Medicago",
-      "Funding": "Medicago",
       "Country/ies": "Canada"
     },
     {
       "Candidate": "DNA-based vaccine",
-      "Sponsor": "Takis Biotech",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Takis Biotech",
-      "Funding": "Takis Biotech",
       "Country/ies": "Italy"
     },
     {
       "Candidate": "Ad26.COV2-S",
-      "Sponsor": "Johnson & Johnson",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Johnson & Johnson",
-      "Funding": "Operation Warp Speed; Johnson & Johnson; BARDA",
       "Country/ies": "USA"
     },
     {
       "Candidate": "AdCOVID",
-      "Sponsor": "Altimmune",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "University of Alabama at Birmingham",
-      "Funding": "Altimmune",
       "Country/ies": "USA"
     },
     {
       "Candidate": "T-COVIDTM",
-      "Sponsor": "Altimmune",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": "Altimmune",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Protein subunit vaccine",
-      "Sponsor": "University of Saskatchewan Vaccine and Infectious Disease Organization-International Vaccine Centre",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "University of Saskatchewan Vaccine and Infectious Disease Organization-International Vaccine Centre",
-      "Funding": "no information",
       "Country/ies": "xx"
     },
     {
       "Candidate": "LUNAR-COV19",
-      "Sponsor": "Arcturus Therapeutics and Duke-NUS Medical School",
-      "Trial Phase": "Pre-clinical",
-      "Institution": "Duke-NUS Medical School, Singapore",
-      "Funding": "Arcturus",
       "Country/ies": "Singapore"
     },
     {
       "Candidate": "Recombinant vesicular stomatitis virus (rVSV) vaccine",
-      "Sponsor": "Merck; IAVI",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": "BARDA",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Adenovirus-based vaccine",
-      "Sponsor": "ImmunityBio; NantKwest",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": null,
       "Country/ies": "USA"
     },
     {
       "Candidate": "Molecular clamp vaccine",
-      "Sponsor": "CSL; The University of Queensland",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": "CEPI; CSL",
       "Country/ies": "Australia"
     },
     {
       "Candidate": "AAVCOVID",
-      "Sponsor": "Massachusetts Eye and Ear; Massachusetts General Hospital; University of Pennsylvania",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": "Wyc Grousbeck; Emilia Fazzalari",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Recombinant vaccine",
-      "Sponsor": "Sanofi, Translate Bio",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": "BARDA",
       "Country/ies": "USA"
     },
     {
       "Candidate": "Covaxin",
-      "Sponsor": "Bharat Biotech; National Institute of Virology",
-      "Trial Phase": "Pre-clinical",
-      "Institution": null,
-      "Funding": "BBIL",
       "Country/ies": "India"
     },
     {
       "Candidate": "mRNA lipid nanoparticle (mRNA-LNP) vaccine",
-      "Sponsor": "CanSino Biologics, Precision NanoSystems",
-      "Trial Phase": "Early research",
-      "Institution": null,
-      "Funding": null,
       "Country/ies": "China"
     },
     {
       "Candidate": "Adenovirus-based vaccine",
-      "Sponsor": "ReiThera; Leukocare; Univercells",
-      "Trial Phase": "Early research",
-      "Institution": null,
-      "Funding": null,
       "Country/ies": "Belgium"
     },
     {
       "Candidate": "gp96-based vaccine",
-      "Sponsor": "Heat Biologics",
-      "Trial Phase": "Early research",
-      "Institution": "University of Miami Miller School of Medicine",
-      "Funding": null,
       "Country/ies": "USA"
     }
   ]
